de8e75931cdefac96accb39874a3a661812fba4d
clement <chunchieh.liang@coolbitx.com>
2020-09-16 15:34:06 +0800

    Updates

---
+++ precache-manifest.56708b28960b1652e0cdbf1bc2d17487.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d98d3f1627d32031389baae6a880ec",
+    "url": "/icx-sdk-sample/index.html"
+  },
+  {
+    "revision": "470a3b7ba0aa173f2cfb",
+    "url": "/icx-sdk-sample/static/css/2.cbecd43c.chunk.css"
+  },
+  {
+    "revision": "73ebdb4f2e8fc8e4835b",
+    "url": "/icx-sdk-sample/static/css/main.d88eee06.chunk.css"
+  },
+  {
+    "revision": "470a3b7ba0aa173f2cfb",
+    "url": "/icx-sdk-sample/static/js/2.9444f13f.chunk.js"
+  },
+  {
+    "revision": "93a587a0b85afbfd925d7b3f820faced",
+    "url": "/icx-sdk-sample/static/js/2.9444f13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ebdb4f2e8fc8e4835b",
+    "url": "/icx-sdk-sample/static/js/main.92ad5032.chunk.js"
+  },
+  {
+    "revision": "3b8a09fecda9936f886e",
+    "url": "/icx-sdk-sample/static/js/runtime-main.4d724f45.js"
+  },
+  {
+    "revision": "d7235b304c6c85654e8e4f65a08cc6cf",
+    "url": "/icx-sdk-sample/static/media/icon.d7235b30.svg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/icx-sdk-sample/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/icx-sdk-sample/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/icx-sdk-sample/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/icx-sdk-sample/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/icx-sdk-sample/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/icx-sdk-sample/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/icx-sdk-sample/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/icx-sdk-sample/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/icx-sdk-sample/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/icx-sdk-sample/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/icx-sdk-sample/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/icx-sdk-sample/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/icx-sdk-sample/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/icx-sdk-sample/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/icx-sdk-sample/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/icx-sdk-sample/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
